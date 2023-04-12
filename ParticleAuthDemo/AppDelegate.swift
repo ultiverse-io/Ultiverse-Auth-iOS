a92f3c1c53f1aca01f4ff9d3f10005d75da34f26
Zhichao Link <sunzhichao@minijoy.work>
2023-04-12 11:49:19 +0800

    update demo.

--- ParticleAuthDemo/AppDelegate.swift
+++ ParticleAuthDemo/AppDelegate.swift
@@ -5,31 +5,27 @@
 //  Created by link on 2022/11/3.
 //
 
-import UIKit
 import ParticleAuthService
 import ParticleNetworkBase
+import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
+
         // Initialize Particle Network
         ParticleNetwork.initialize(config: .init(chainInfo: .ethereum(.mainnet), devEnv: .debug))
-        
+
         return true
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         if LoginManager.shared.canHandle(url: url) {
             return true
         } else {
             return ParticleAuthService.handleUrl(url)
         }
     }
-
-    
 }
-
